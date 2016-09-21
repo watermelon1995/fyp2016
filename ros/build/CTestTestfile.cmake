b2445d20139b87ae27262c48aaad34dc2bdff37c
watermelon1995 <qoo238302@gmail.com>
2016-09-21 15:54:44 +0800

    playing with ros publish and subscriber

---
+++ ros/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/kin/fyp2016/ros/src
+# Build directory: /home/kin/fyp2016/ros/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+SUBDIRS(gtest)
+SUBDIRS(first_try)
---
+++ ros/build/first_try/CMakeFiles/listener.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/listener.dir/src/helloworld_listener.cpp.o"
+  "/home/kin/fyp2016/ros/devel/lib/first_try/listener.pdb"
+  "/home/kin/fyp2016/ros/devel/lib/first_try/listener"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/listener.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
